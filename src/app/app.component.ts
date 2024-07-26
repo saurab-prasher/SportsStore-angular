0b5154f0949ef9c85a5cef7dbcdb77047ff8e678
saurabprasher <saurab015@gmail.com>
2024-07-26 16:52:46 -0400

    pagingation and displaying of products done

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+
+  // template: `
+  //   <div class="bg-success p-2 text-center text-white">This is SportsStore</div>
+  // `,
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'SportsStore';
+}
---
+++ src/app/store/store.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { ProductRepository } from '../model/product.repository';
+import { Product } from '../model/product.model';
+
+@Component({
+  selector: 'store',
+  templateUrl: './store.component.html',
+})
+export class StoreComponent {
+  selectedCategory: string | undefined;
+  productsPerPage = 4;
+  selectedPage = 1;
+  constructor(private repository: ProductRepository) {}
+
+  get products(): Product[] {
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
+  }
+  get categories(): string[] {
+    return this.repository.getCategories();
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+
+  get pageCount(): number {
+    return Math.ceil(
+      this.repository.getProducts(this.selectedCategory).length /
+        this.productsPerPage
+    );
+  }
+
+  changeCategory(newCategory?: string) {
+    this.selectedCategory = newCategory;
+  }
+}
